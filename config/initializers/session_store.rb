08a85b0f5c70e1ecc3fc22925cf6539b70d7388d
nd0ut <nd0ut.me@gmail.com>
2013-04-24 02:43:34 +0400

    session name changed

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ShortTheRails::Application.config.session_store :cookie_store, key: '_url_shortener_session'
+ShortTheRails::Application.config.session_store :cookie_store, key: '_short_the_rails_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
