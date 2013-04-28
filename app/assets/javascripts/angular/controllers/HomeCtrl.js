4aea651b9268a1beee63b7272d695f67774681e3
nd0ut <nd0ut.me@gmail.com>
2013-04-29 01:08:21 +0400

    short form improvement

--- app/assets/javascripts/angular/controllers/HomeCtrl.js
+++ app/assets/javascripts/angular/controllers/HomeCtrl.js
@@ -1,22 +1,32 @@
-angular.module('app.controllers').controller('HomeCtrl', function NavBarCtrl($scope, Session, Url) {
+angular.module('app.controllers').controller('HomeCtrl', function NavBarCtrl($scope, Url) {
     $scope.url = new Url();
 
     $scope.short = function () {
-        $scope.url.url = $scope.protocol + $scope.url.url;
-        $scope.url.$save();
+        var url = $scope.url.url;
+
+        $scope.url.$save(function(u) {
+            if (u.error) {
+                alert(u.error);
+            }
+
+            u.url = url;
+        });
+
         console.log($scope.url);
     }
 
     $scope.onUrlChange = function() {
-        var protocol_index = $scope.url.url.indexOf('://');
+        var protocol_index = $scope.url_without_protocol.indexOf('://');
 
         if(protocol_index != -1) {
             protocol_index = protocol_index + 3;
 
-            if(protocol_index != $scope.url.url.length) {
-                $scope.protocol = $scope.url.url.substr(0, protocol_index);
-                $scope.url.url = $scope.url.url.substr(protocol_index);
+            if(protocol_index != $scope.url_without_protocol.length) {
+                $scope.protocol = $scope.url_without_protocol.substr(0, protocol_index);
+                $scope.url_without_protocol = $scope.url_without_protocol.substr(protocol_index);
             }
         }
+
+        $scope.url.url = $scope.protocol + $scope.url_without_protocol;
     };
 });
